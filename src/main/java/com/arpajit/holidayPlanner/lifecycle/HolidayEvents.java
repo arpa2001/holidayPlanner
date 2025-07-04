b2bae3be3c8b08d2cc23d36d11273dd341a1b24f
arpa2001 <arpajitofficial@gmail.com>
2025-07-04 18:01:26 +0530

    added shutdown logs

--- src/main/java/com/arpajit/holidayPlanner/lifecycle/HolidayEvents.java
+++ src/main/java/com/arpajit/holidayPlanner/lifecycle/HolidayEvents.java
@@ -1,16 +1,22 @@
 package com.arpajit.holidayPlanner.lifecycle;
 
-import org.slf4j.*;
+import org.apache.logging.log4j.*;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.event.*;
 import org.springframework.stereotype.Component;
+import jakarta.annotation.PreDestroy;
 
 @Component
 public class HolidayEvents {
-    private static final Logger logger = LoggerFactory.getLogger(HolidayEvents.class);
+    private static final Logger logger = LogManager.getLogger(HolidayEvents.class);
 
     @EventListener
     public void startupSuccess(ApplicationReadyEvent event) {
         logger.info("Ready to accept requests");
+    }
+
+    @PreDestroy
+    public void appShutdown() {
+        logger.fatal("Application is shutting down...");
     }
 }
