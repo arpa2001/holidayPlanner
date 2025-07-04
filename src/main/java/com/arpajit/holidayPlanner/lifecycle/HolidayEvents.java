7a1dedebb1126859830fe3b77f3ebaf8fba05727
arpa2001 <arpajitofficial@gmail.com>
2025-07-04 12:39:25 +0530

    logs updated

---
+++ src/main/java/com/arpajit/holidayPlanner/lifecycle/HolidayEvents.java
@@ -0,0 +1,16 @@
+package com.arpajit.holidayPlanner.lifecycle;
+
+import org.slf4j.*;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HolidayEvents {
+    private static final Logger logger = LoggerFactory.getLogger(HolidayEvents.class);
+
+    @EventListener
+    public void logStartupSuccess(ApplicationReadyEvent event) {
+        logger.info("Completed App Startup successfully");
+    }
+}
